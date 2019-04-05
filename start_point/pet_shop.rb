f84a9fbe5bc44a7446099011950bde97197a031e
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-05 13:37:17 +0100

    pet_shop:test 4 & 5 passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,9 +1,19 @@
-def pet_shop_name(pet_shop)
-  pet_shop_name = pet_shop[:name]
-  return pet_shop_name
+def pet_shop_name(business)
+  business_name = business[:name]
+  return business_name
 end
 
-def total_cash(pet_shop)
-  pet_shop_monies_total = pet_shop[:admin][:total_cash]
-  return pet_shop_monies_total
+def total_cash(business)
+  business_monies_total = business[:admin][:total_cash]
+  return business_monies_total
+end
+
+
+def add_or_remove_cash(business,amount_to_be_added_removed)
+  business[:admin][:total_cash] += amount_to_be_added_removed
+end
+
+
+def pets_sold(business)
+  return business[:admin][:pets_sold]
 end
